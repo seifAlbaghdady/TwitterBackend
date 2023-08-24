c09d42f484140771747a517e422187f1c9c7ef83
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2023-08-24 04:22:45 +0300

    Update TweetRoutes.js

--- src/routes/TweetRoutes.js
+++ src/routes/TweetRoutes.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+const { PrismaClient } = require('@prisma/client');
+const { error } = require('console');
+const prisma = new PrismaClient();
 //Create Tweet
-router.post('/', (req, res) => {
-    res.status(200).json({message: "Tweet created"});
+router.post('/', async (req, res) => {
+    try{
+        const {content,image,UserID}=req.body
+        const newtweet=await prisma.tweet.create({data:{content,image,UserID}})
+        res.status(200).json(newtweet);
+    }catch(err)
+    {
+        res.status(400).json({error : "Cant Tweet"})
+    }
 });
 
 // Get all tweets
-router.get('/', (req, res) => {
-    res.status(200).json({message: "All tweets"});
+router.get('/', async (req, res) => {
+    try{
+        const alltweets=await prisma.tweet.findMany();
+        res.status(200).json(alltweets);
+    }catch(err)
+    {
+        res.status(400).json({error:"Somthing wrong"})
+    }
+    
 });
 
 // Get one tweet by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const id=req.params.id;
-    res.status(200).json({message: `Tweet by id= ${id}`});
+    const tweet=await prisma.tweet.findUnique({where:{id:Number(id)}})
+    if(tweet){
+        res.status(200).json(tweet);
+    }
+    else{
+        res.status(400).json({error:"There is no tweet with this id"})
+    }
 });
 
 // Delete Tweet
-router.delete('/:id',(req,res)=>{
+router.delete('/:id',async (req,res)=>{
     const id=req.params.id;
+    await prisma.tweet.delete({where:{id:Number(id)}});
     res.status(200).json({message: `Tweet Deleted by id= ${id}`})
 })
 
